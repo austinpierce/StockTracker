9d52c14ebbd49062f5b685807892befe40a27469
Austin Pierce <AustinPierce@gmail.com>
2018-07-01 18:58:06 -0400

    Fully functional search and add stock

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,6 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
   
 #  def current_price(ticker_symbol)
  #   @currentPrice = StockQuote:Stock.quote(ticker_symbol).latest_price  
@@ -11,6 +13,10 @@ class Stock < ApplicationRecord
     rescue Exception => e
       return nil
     end
+  end
+  
+  def self.find_by_ticker(ticker_symbol)
+    where(ticker: ticker_symbol).first
   end
   
 end
