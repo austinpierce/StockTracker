1825171e4040aec0e774351a30585162455b9752
Austin Pierce <AustinPierce@gmail.com>
2018-07-02 19:55:44 -0400

    Display quantity and purchase price in _list

--- db/migrate/20180702161835_add_columns_to_user_stocks.rb
+++
@@ -1,6 +0,0 @@
-class AddColumnsToUserStocks < ActiveRecord::Migration[5.1]
-  def change
-    add_column :user_stocks, :quantity, :integer
-    add_column :user_stocks, :purchase_price, :decimal
-  end
-end
